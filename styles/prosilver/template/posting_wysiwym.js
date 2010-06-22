f1a3d92330185667448a1d5ad2295bc5c07e13c9
Gabriel <forum@mssti.com>
2010-06-22 17:26:55 -0300

    ommited Version 0.0.4

--- styles/prosilver/template/posting_wysiwym.js
+++ styles/prosilver/template/posting_wysiwym.js
@@ -1,20 +1,41 @@
 /**
 * @package: phpBB 3.0.7-PL1 :: WYSIWYM Markdown editor -> root/styles/prosilver/template
-* @version: $Id: posting_wysiwym.js, [DEV] 0.0.3 2010/06/02 10:06:02 leviatan21 Exp $
+* @version: $Id: posting_wysiwym.js, [DEV] 0.0.4 2010/06/02 10:06:02 leviatan21 Exp $
 * @copyright: leviatan21 < info@mssti.com > (Gabriel) http://www.mssti.com/phpbb3/
 * @license: http://opensource.org/licenses/gpl-license.php GNU Public License 
 * @author: leviatan21 - http://www.phpbb.com/community/memberlist.php?mode=viewprofile&u=345763
 * 
 **/
 
+/**
+* @ignore 
+*
+* @todo :
+*	improve code ?
+*	improve url detection ?
+*	attachment improve
+*	improve phpbb_tokens
+*
+* @done :
+*	font size & font color click.
+*	Maximum nested quotes per post
+*	images optionget
+*	flash optionget
+*	Do not automatically parse URLs
+**/
+
+
 /** Some css on-the-fly - Start **/
 document.write("\n\r" + '<style type="text/css" media="all">'+ "\r" + '<!--' + "\r");
-document.write("\r" + '#wysiwim_preview .postbody { height: auto; font-size: 85%; }');
+document.write("\r" + '#wysiwim_preview .postbody { height: auto; font-size: 99%; }');
 document.write("\r" + '#wysiwim_editor { overflow: auto; height: 200px; padding-{S_CONTENT_FLOW_END}: 2px; }');
 /** Style dependance - Start **/
 document.write("\r" + '#wysiwim_editor .codebox { padding: 3px; background-color: #FFFFFF; border: 1px solid #d8d8d8; }');
-document.write("\r" + '#wysiwim_editor .codebox div { float: none; font-weight: bold; font-size: 0.8em; text-transform: uppercase; border-bottom: 1px solid #cccccc; }');
-document.write("\r" + '#wysiwim_editor .codebox code { color: #2E8B57; white-space: normal; display: block; }');
+document.write("\r" + '#wysiwim_editor .codebox div { font-weight: bold; font-size: 0.8em; text-transform: uppercase; border-bottom: 1px solid #cccccc; }');
+document.write("\r" + '#wysiwim_editor .codebox code { color: #2E8B57; white-space: normal; display: block; font: 0.9em Monaco, "Andale Mono","Courier New", Courier, mono; }');
+document.write("\r" + '#wysiwim_editor blockquote div { height: auto; width: 100%; }');
+document.write("\r" + '#wysiwim_editor .error { font-size: 1em; }');
+// document.write("\r" + '#wysiwim_editor .postlink-local { color: red; }');
 
 /** Style dependance - End **/
 document.write( "\r" + '-->' + "\r" + '</style>' + "\n\r");
@@ -28,6 +49,11 @@ var WYSIWYM = new function()
 	var phpbb_editor = '';
 	var wysiwim_editor = '';
 	var wysiwim_preview = 'wysiwim_preview';
+	var max_quote_depth = <!-- IF W_MAX_QUOTE_DEEP -->{W_MAX_QUOTE_DEEP}<!-- ELSE -->3<!-- ENDIF -->;
+	var viewimg = <!-- IF W_VIEW_IMAGES -->{W_VIEW_IMAGES}<!-- ELSE -->1<!-- ENDIF -->;
+	var viewflash = <!-- IF W_VIEW_FLASH -->{W_VIEW_FLASH}<!-- ELSE -->1<!-- ENDIF -->;
+	var viewsmilies = <!-- IF W_VIEW_SMILIES -->{W_VIEW_SMILIES}<!-- ELSE -->1<!-- ENDIF -->;
+	var display_warning = false;
 
 	/**
 	* Set some default variables and hide the preview
@@ -47,7 +73,6 @@ var WYSIWYM = new function()
 	BBcode_set = function()
 	{
 		var BBcode = {
-			/* @todo improve */
 			'code' : {
 				clear_open : '[code]',
 				clear_close : '[/code]',
@@ -56,13 +81,12 @@ var WYSIWYM = new function()
 				html_open : bbcode_code,
 				html_close : ''
 			},
-			/* @todo Maximum nested quotes per post */
 			'quote_username' : {
 				clear_open : '[quote=',
 				clear_close : '[/quote]',
 				bbcode_open : /\[quote="(.*?)"\]/gim,
 				bbcode_close : /\[\/quote\]/gim,
-				html_open : '<blockquote><div style="height: auto; width: 100%;"><cite>$1 {L_WROTE}:</cite>',
+				html_open : '<blockquote><div><cite>$1 {L_WROTE}:</cite>',
 				html_close : '</div></blockquote>'
 			},
 			'quote_uncited' : {
@@ -70,7 +94,7 @@ var WYSIWYM = new function()
 				clear_close : '[/quote]',
 				bbcode_open : /\[quote\]/gim,
 				bbcode_close : /\[\/quote\]/gim,
-				html_open : '<blockquote class="uncited"><div style="height: auto; width: 100%;">',
+				html_open : '<blockquote class="uncited"><div>',
 				html_close : '</div></blockquote>'
 			},
 			'bold' : {
@@ -89,12 +113,11 @@ var WYSIWYM = new function()
 				html_open : '<em>',
 				html_close : '</em>'
 			},
-			/* @todo improve url detection ? */
 			'url' : {
 				clear_open : '[url]',
 				clear_close : '[/url]',
 				bbcode_open : /\[url\](ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?\[\/url\]/gim,
-			//	/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+			//						 /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
 				bbcode_close : '',
 				html_open : '<a href="$3" class="postlink">$3</a>',
 				html_close : ''
@@ -108,13 +131,20 @@ var WYSIWYM = new function()
 				html_open : '<a href="$3" class="postlink">$7</a>',
 				html_close : ''
 			},
-			/* @todo optionget */
 			'image' : {
 				clear_open : '[img]',
 				clear_close : '[/img]',
 				bbcode_open : /\[img\](.*?)\[\/img\]/gim,
 				bbcode_close : '',
 				html_open : '<img src="$1" alt="{L_IMAGE}" class="resize_me" />',
+				html_close : ''
+			},
+			'image_viewimg' : {
+				clear_open : '[img]',
+				clear_close : '[/img]',
+				bbcode_open : /\[img\](.*?)\[\/img\]/gim,
+				bbcode_close : '',
+				html_open : ' <a href="$1" alt=">[ img ]" />[ img ]</a> ',
 				html_close : ''
 			},
 			'size' : {
@@ -175,13 +205,20 @@ var WYSIWYM = new function()
 				html_open : '<a href="mailto:$1" class="postlink">$2</a>',
 				html_close : ''
 			},
-			/* @todo optionget */
 			'flash' : {
 				clear_open : '[flash=',
 				clear_close : '[/flash]',
 				bbcode_open : /\[flash=(\d+),(\d+)\](.*?)\[\/flash\]/gim,
 				bbcode_close : '',
 				html_open : '<object classid="clsid:D27CDB6E-AE6D-11CF-96B8-444553540000" codebase="http://active.macromedia.com/flash2/cabs/swflash.cab#version=5,0,0,0" width="$1" height="$2"><param name="movie" value="$3" /><param name="play" value="false" /><param name="loop" value="false" /><param name="quality" value="high" /><param name="allowScriptAccess" value="never" /><param name="allowNetworking" value="internal" /><embed src="$3" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="$1" height="$2" play="false" loop="false" quality="high" allowscriptaccess="never" allownetworking="internal"></embed></object>',
+				html_close : ''
+			},
+			'flash_viewflash' : {
+				clear_open : '[flash=',
+				clear_close : '[/flash]',
+				bbcode_open : /\[flash=(\d+),(\d+)\](.*?)\[\/flash\]/gim,
+				bbcode_close : '',
+				html_open : ' <a href="$3" alt="[ flash ]" />[ flash ]</a> ',
 				html_close : ''
 			},
 <!-- IF .wysiwim_custom_tags -->
@@ -219,7 +256,6 @@ var WYSIWYM = new function()
 			},
 **/
 <!-- ENDIF -->
-			/* @todo improve */
 			'attachment' : {
 				clear_open : '[attachment=',
 				clear_close : '[/attachment]',
@@ -303,6 +339,7 @@ var WYSIWYM = new function()
 		return smiley;
 	};
 <!-- ENDIF -->
+
 	/**
 	* Parse code text from code tag
 	**/
@@ -312,11 +349,11 @@ var WYSIWYM = new function()
 		content = content.replace(/  /g, "&nbsp;&nbsp;");
 		/* Tabs to 4 spaces */
 		content = content.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
-		/*	'['	to 	'&#91;'	and ']'	to 	'&#93;'		*/
+		/*	'['	to	'&#91;'	and	']'	to	'&#93;'		*/
 		content = content.replace(/\[/g, '&#091;').replace(/\]/g, '&#093;');
-		/*	'.'	to 	'&#46;'	and	':'	to 	'&#058;'	*/
+		/*	'.'	to	'&#46;'	and	':'	to	'&#058;'	*/
 		content = content.replace(/\./g, '&#046;').replace(/\:/g, '&#058;');
-		/*	'<'	to 	'&lt;'	and	'>'	to 	'&gt;'		*/
+		/*	'<'	to	'&lt;'	and	'>'	to	'&gt;'		*/
 		content = content.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
 		return '<div class="codebox">' + '<div>{L_CODE}: <a href="#" onclick="selectCode(this); return false;">{L_SELECT_ALL_CODE}</a></div><code>' + content + '</code></div>';
@@ -433,6 +470,92 @@ var WYSIWYM = new function()
 		return false;
 	};
 
+	is_checked = function(id)
+	{
+		if (document.getElementById(id))
+		{
+			return document.getElementById(id).checked;
+		}
+		return false;
+	};
+
+	/**
+	* http://tools.ietf.org/html/rfc3986
+	* http://www.codingforums.com/archive/index.php/t-9241.html
+	*
+	*	An example URIs and their component parts:
+	*	foo://example.com:8042/over/there?name=ferret#nose
+	*	\_/   \______________/\_________/ \_________/ \__/
+	*	 |           |            |            |        |
+	* scheme     authority       path        query   fragment
+	**/
+	parse_magic_url = function()
+	{
+		//	http://www.mssti.com/phpbb3/viewtopic.php?f=95&p=10994#p10994
+		//	viewtopic.php?f=70&t=2093183&start=0
+		var full_url	= arguments[0];
+		var scheme		= arguments[1];		//	http://			||
+		var before		= (arguments[2] ? arguments[2] : '');
+		var authority	= arguments[4];		//	www.mssti.com	||	viewtopic.php
+		var script		= arguments[11];	//	/viewtopic.php	||
+		var query		= arguments[13];	//	?f=95&p=10994	||	?f=70&t=2093183&start=0
+		var fragment	= arguments[16];	//	#p10994			||
+		var after		= (arguments[18] ? arguments[18] : '');
+		var css_class = 'postlink';
+
+		var url = full_url.replace(/( |\n)/g, "");
+		var short_url = (url.length > 55) ? url.substring(0, 39) + ' ... ' + url.substring(url.length, url.length-10) : url;
+
+		// Means it's not local
+		if (!scheme && script)
+		{
+			url = 'http://' + url;
+		}
+		// Means it's local
+		if (!script && query)
+		{
+			css_class = 'postlink-local';
+		}
+		/**
+		var toalert = '';
+		for(var i=0; i<arguments.length; i++)
+		{
+			toalert += "argument=("+i+") value=("+arguments[i]+")"+"\n";
+		}
+		alert(toalert);
+		**/
+		return before + '<a href="' + url + '" class="' + css_class + '">' + short_url + '</a>' + after;
+	};
+
+	/**
+	* JavaScript preg_quote
+	*	Quote regular expression characters plus an optional character 
+	* Code from : http://phpjs.org/functions/preg_quote:491
+	RegExp_escape = function(str)
+	{
+		var specials = new RegExp("[.*+?|()\\[\\]{}\\\\]", "g"); // .*+?|()[]{}\
+		return str.replace(specials, "\\$&");
+		return str;
+	};
+	**/
+	preg_quote = function(str)
+	{
+		// Quote regular expression characters plus an optional character
+		// 
+		// version: 1004.2314
+		// discuss at: http://phpjs.org/functions/preg_quote
+		// +   original by: booeyOH
+		// +   improved by: Ates Goral (http://magnetiq.com)
+		// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		// +   bugfixed by: Onno Marsman
+		// *     example 1: preg_quote("$40");    // *     returns 1: '\$40'
+		// *     example 2: preg_quote("*RRRING* Hello?");
+		// *     returns 2: '\*RRRING\* Hello\?'
+		// *     example 3: preg_quote("\\.+*?[^]$(){}=!<>|:");
+		// *     returns 3: '\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:'
+		return (str+'').replace(/([\\\.\+\*\?\[\^\]\$\(\)\{\}\=\!<>\|\:])/g, '\\$1');
+	};
+
 	/**
 	* build a valid regexp
 	**/
@@ -544,18 +667,41 @@ var WYSIWYM = new function()
 	**/
 	bbcode_to_html_Parse = function(str)
 	{
+		/* Do not automatically parse URLs - Start */
+		if (!is_checked('disable_magic_url'))
+		{//	http://www.weberdev.com/get_example-4569.html
+			var url_match = /((\s|\n)(http|https|ftp):\/\/)?((([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w][-\d\w]{0,253}[\d\w]\.)+[\w]{2,4}(:[\d]+)?)(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?(\s|\n)/gim;
+			/* I swear I try to made this easy, but can't */
+			str = str.replace(url_match, parse_magic_url);
+		}
+		/* Do not automatically parse URLs - End */
+
 		/** Have the post a possible bbcode ? **/
-		if (str.indexOf('[') < -1)
+		if (str.indexOf('[') < 0 || is_checked('disable_bbcode'))
 		{
+			/* convert CRLF to <br> */
+			str = str.replace(/\n/g, "<br />");
 			return str;
 		}
 
+		var bbcode_start;
+		var bbcode_end;
 		var bbcodes = BBcode_set();
 		for (var i in bbcodes)
 		{
 			// Have this bbcode (open or close tag)?
-			if (str.indexOf(bbcodes[i].clear_open) > -1 || (str.indexOf(bbcodes[i].clear_close) > -1))
+			bbcode_start = (isset(bbcodes[i].bbcode_open)  ? str.indexOf(bbcodes[i].clear_open)  > -1 : false);
+			bbcode_end = (isset(bbcodes[i].bbcode_close) ? str.indexOf(bbcodes[i].clear_close) > -1 : (bbcode_start ? true : false));
+			if (bbcode_start && bbcode_end)
+//			if (str.indexOf(bbcodes[i].clear_open) > -1 || (str.indexOf(bbcodes[i].clear_close) > -1))
 			{
+				/*	images optionget and flash optionget - Start */
+				if ((i == 'image' && !viewimg) || (i == 'flash' && !viewflash))
+				{
+					continue;
+				}
+				/*	images optionget and flash optionget - End */
+				
 				// Replace the bbcode open with his corresponding open html
 				str = str.replace(bbcodes[i].bbcode_open, bbcodes[i].html_open);
 
@@ -566,6 +712,14 @@ var WYSIWYM = new function()
 				}
 			}
 
+			/* Check for max_quote_depth - Start */
+			if (max_quote_depth > 0 && bbcodes[i].clear_close == '[/quote]' && bbcode_start && bbcode_end)
+			{
+				str = bbcode_quote_depth(str, bbcodes[i]);
+			}
+			/* Check for max_quote_depth - End */
+
+			/* Parse custom bbcodes - Start */
 		<!-- IF .wysiwim_custom_tags -->
 			if (isset(bbcodes[i].custom_tag))
 			{
@@ -579,25 +733,31 @@ var WYSIWYM = new function()
 				}
 			}
 		<!-- ENDIF -->
+			/* Parse custom bbcodes - End */
 		}
 
-		<!-- IF S_SMILIES_ALLOWED and .smiley -->
-		var smilies = phpbb_smilies();
-		for (var smiley in smilies)
+		/* Parse posting smilies - Start */
+		if (viewsmilies && !is_checked('disable_smilies'))
 		{
-			// Have this smiley ?
-			if (str.indexOf(smiley) > -1)
+			<!-- IF S_SMILIES_ALLOWED and .smiley -->
+			var smilies = phpbb_smilies();
+			for (var smiley in smilies)
 			{
-				str = str.replace(smiley, ' <img src="' + smilies[smiley].image + '" alt="' + smiley + '" title="' + smilies[smiley].description + '" /> ');
-			}			
+				smilie = new RegExp(preg_quote(smiley), 'g');
+				// Have this smiley ?
+				if (str.indexOf(smiley) > -1)
+				{
+					str = str.replace(smilie, ' <img src="' + smilies[smiley].image + '" alt="' + smiley + '" title="' + smilies[smiley].description + '" /> ');
+				}			
+			}
+			<!-- ENDIF -->
 		}
-		<!-- ENDIF -->
-
+		/* Parse posting smilies - End */
 
 		/* Back to []:. */
-		//	'&#091;'	to	'['	and	'&#093;'	to 	']'
+		//	'&#091;'	to	'['	and	'&#093;'	to	']'
 		str = str.replace(/&amp;#091;/gi, '[').replace(/&amp;#093;/gi, ']');
-		//	'&#046;'	to 	'.' and '&#058;'	to	':'
+		//	'&#046;'	to	'.'	and	'&#058;'	to	':'
 		str = str.replace(/&amp;#046;/gi, '.').replace(/&amp;#058;/gi, ':');
 
 		/* Lists have too much "carriage return"	*/
@@ -610,6 +770,48 @@ var WYSIWYM = new function()
 		str = str.replace(/\n/g, "<br />");
 
 		return str + "<br />";
+	};
+
+	/*
+	* Check for max_quote_depth
+	*/
+	bbcode_quote_depth = function(str, bbcodes)
+	{
+		var quote_count = str.split(bbcodes.html_close).length - 1;
+
+		if (max_quote_depth > 0 && quote_count > max_quote_depth)
+		{
+			var quote_start = 0;
+			var quote_end = 0;
+			for (var i = 0, str_length = str.length; i < str_length; i++)
+			{
+				quote_start = str.indexOf(bbcodes.html_open, i);
+				quote_end = str.indexOf(bbcodes.html_close, i);
+				if (quote_start > -1 && quote_end > quote_start)
+				{
+					str_quote = str.substring(quote_start, quote_end + bbcodes.html_close.length);
+					var quote_count = str_quote.split('<blockquote').length - 1;
+
+					// there are too many nested quotes
+					if (max_quote_depth > 0 && quote_count > max_quote_depth)
+					{
+						var warning = '{LA_QUOTE_DEPTH_EXCEEDED}'.replace( '%1$d', max_quote_depth);
+						var last_quote_start = str_quote.lastIndexOf('<blockquote');
+						var last_quote_end = str_quote.lastIndexOf('blockquote>');
+						var last_quote_old = str_quote.substring(last_quote_start, last_quote_end + 11);
+						var last_quote_new = '<p class="error">' + warning + '</p>';
+						str = str.replace(last_quote_old, last_quote_new);
+						if (display_warning)
+						{
+							alert(warning);
+						}
+						break;
+					}
+					i = quote_start;
+				}
+			}
+		}
+		return str;
 	};
 
 	this.Start = Start;
